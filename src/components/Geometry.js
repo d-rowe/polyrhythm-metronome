8234f8249ce00412eee0ca370f0ffa9b0fca8fa2
Daniel Rowe <danielrowemusic@gmail.com>
2019-04-24 20:10:33 -0700

    Beat translation animations working in individual segments
    Small refactoring

--- src/components/Geometry.js
+++ src/components/Geometry.js
@@ -1,31 +1,54 @@
 import React from "react";
 import Two from "two.js";
-import { TweenMax, Expo } from "gsap/TweenMax";
+import { TweenMax, SlowMo } from "gsap/TweenMax";
 
 class Geometry extends React.Component {
+  componentDidMount() {
+    this.initializeTwo();
+    this.drawShapes(3, 5, 100);
+    this.initiateBeatCircles();
+    this.startAnimation();
+  }
+
   initializeTwo() {
     var params = { width: 1000, height: 500, autostart: true };
     this.two = new Two(params).appendTo(this.refs.geoCanvas);
-    this.drawShapes(3, 5, 100);
+    this.origin = { x: 200, y: 200 };
   }
 
   drawShapes(sides1, sides2, radius) {
     var lineWidth = 2;
     var outerRadius = radius * 1.5;
-    var shape1 = this.two.makePolygon(200, 200, radius, sides1);
-    var shape2 = this.two.makePolygon(200, 200, outerRadius, sides2);
-    var circle1 = this.two.makeCircle(200, 200, radius);
-    var circle2 = this.two.makeCircle(200, 200, outerRadius);
+    var shape1 = this.two.makePolygon(
+      this.origin.x,
+      this.origin.y,
+      radius,
+      sides1
+    );
+    var shape2 = this.two.makePolygon(
+      this.origin.x,
+      this.origin.y,
+      outerRadius,
+      sides2
+    );
+    var circle1 = this.two.makeCircle(this.origin.x, this.origin.y, radius);
+    var circle2 = this.two.makeCircle(
+      this.origin.x,
+      this.origin.y,
+      outerRadius
+    );
 
     shape1.stroke = "orangered";
     shape1.linewidth = lineWidth;
     shape1.rotation = Math.PI + Math.PI / sides1;
     shape1.noFill();
+    shape1.points = this.getPoints(shape1, sides1);
 
     shape2.stroke = "rgb(0, 200, 255)";
     shape2.linewidth = lineWidth;
     shape2.rotation = Math.PI + Math.PI / sides2;
     shape2.noFill();
+    shape2.points = this.getPoints(shape2, sides2);
 
     circle1.linewidth = lineWidth;
     circle1.opacity = 0;
@@ -37,42 +60,71 @@ class Geometry extends React.Component {
 
     this.shape1 = shape1;
     this.shape2 = shape2;
-
-    this.initiateBeatCircles();
-    this.two.update();
   }
 
   initiateBeatCircles() {
-    console.log(this.shape1.vertices);
-
-    var beatCircle1 = this.two.makeCircle(200, 100, 10);
-    beatCircle1.fill = "orangered";
-    beatCircle1.noStroke();
-
-    var beatCircle2 = this.two.makeCircle(200, 50, 10);
-    beatCircle2.fill = "rgb(0, 200, 255)";
-    beatCircle2.noStroke();
-    // console.log(beatCircle1);
-
-    TweenMax.to(beatCircle1, 0.4, {
-      radius: 20,
-      x: "10",
-      ease: Expo.easeInOut,
-      yoyo: true,
-      repeat: -1
-    });
-    TweenMax.to(beatCircle2, 0.4, {
-      radius: 20,
-      ease: Expo.easeInOut,
-      yoyo: true,
-      repeat: -1
-    });
-    // this.two.bind('update', (frameCount) ->
-    // ).play()
+    this.beatCircle1 = this.two.makeCircle(200, 100, 15);
+    this.beatCircle1.fill = "orangered";
+    this.beatCircle1.noStroke();
+    this.beatCircle2 = this.two.makeCircle(200, 50, 10);
+    this.beatCircle2.fill = "rgb(0, 200, 255)";
+    this.beatCircle2.noStroke();
   }
 
-  componentDidMount() {
-    this.initializeTwo();
+  startAnimation() {
+    TweenMax.to(this.beatCircle1.translation, 1, {
+      x: this.shape1.points[1].x,
+      y: this.shape1.points[1].y,
+      yoyo: true,
+      repeat: -1
+    });
+
+    TweenMax.to(this.beatCircle1, 1, {
+      radius: 5,
+      ease: SlowMo.easeInOut,
+      yoyo: true,
+      repeat: -1
+    });
+
+    TweenMax.to(this.beatCircle2.translation, 1, {
+      x: this.shape2.points[1].x,
+      y: this.shape2.points[1].y,
+      yoyo: true,
+      repeat: -1
+    });
+
+    TweenMax.to(this.beatCircle2, 1, {
+      radius: 5,
+      ease: SlowMo.easeInOut,
+      yoyo: true,
+      repeat: -1
+    });
+  }
+
+  getPoints(shape, sides) {
+    let points = [];
+    for (let i = 0; i < sides; i++) {
+      let vertex = shape.vertices[i];
+      let point = this.rotatePoints(
+        this.origin.x,
+        this.origin.y,
+        this.origin.x - vertex.x,
+        this.origin.y - vertex.y,
+        180 / sides
+      );
+      points.push({ x: point[0], y: point[1] });
+    }
+    points.push(points.shift());
+    return points.reverse();
+  }
+
+  rotatePoints(cx, cy, x, y, angle) {
+    let radians = (Math.PI / 180) * angle;
+    let cos = Math.cos(radians);
+    let sin = Math.sin(radians);
+    let nx = cos * (x - cx) + sin * (y - cy) + cx;
+    let ny = cos * (y - cy) - sin * (x - cx) + cy;
+    return [nx, ny];
   }
 
   render() {
